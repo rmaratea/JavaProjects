d3d4333f8991e7824ab41313b7bd0f203fa29972
rmaratea <roberto.maratea@gmail.com>
2017-07-01 18:34:39 +0200

    Test commit to verify Jenkins #3

---
+++ anubi.common/src/main/scala/HelloWorld.scala
@@ -0,0 +1,11 @@
+package main.scala
+
+/**
+  * Created by Roberto on 01/07/2017.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
+
--- anubi.common/src/main/scala/main.scala
+++
@@ -1,9 +0,0 @@
-package main.scala
-
-/**
-  * Created by Roberto on 01/07/2017.
-  */
-class fileReader {
-
-
-}
